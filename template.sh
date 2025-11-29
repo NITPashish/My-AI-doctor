4e851e6efc05681168858fce15d5a4e441cc925d
NITPashish <ashishr.ug20.me@nitp.ac.in>
2025-11-29 10:29:48 +0530

    Folder strcutured creaed

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating directories..
+
+mkdir src
+mkdir experiments
+
+# creating files..
+touch src/__init__.py
+touch src/helper.py
+touch .env
+touch setup.py
+touch src/prompt.py
+touch app.py
+touch requirements.txt
+touch experiments/testing.ipynb
+
+echo "files and folders created successfully"
